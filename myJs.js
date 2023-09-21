06cfcfdf8c92e1da1ed66480edf4b4fa78896137
i-Am-Mishka <istislavistislav@gmail.com>
2023-09-21 11:54:20 +0300

    first commit

---
+++ myJs.js
@@ -0,0 +1,51 @@
+const arr = [12,23,34,56];
+
+const black = () => {
+for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i])
+    if (!(arr[i] % 2)) {
+        console.log(arr[i], '<--')
+    } else {
+        console.log(arr[i], '>++')   }
+}
+
+}
+black()
+console.log(2%2)
+console.log(3%2)
+console.log(false == 0)
+console.log(false === 0)
+console.log(true == 1)
+
+const arr2 = [-1, -5 , 0, 3, 4, 1.4, -10]
+
+const yellow = () => {
+    for (let i = 0; i < arr2.length; i++) {
+        //  console.log(arr2[i])
+         if(arr2[i] > 0) {
+            console.log(arr2[i])
+         }
+    }
+}
+yellow()
+
+// console.log(arr2[0])
+// console.log(arr2[1])
+// console.log(arr2[2])
+// console.log(arr2[3])
+// console.log(arr2[4])
+// console.log(arr2[5])
+
+const orenge = [2, 'five',  8, 'nine', false, 'one']
+
+const rose = () => {
+
+    for (let i = 0; i < orenge.length; i++) {
+        // console.log(typeof orenge[i] === 'string' )
+        if(typeof orenge[i] === 'string') {
+            console.log (orenge[i])
+        }
+    }
+}
+rose()
+
